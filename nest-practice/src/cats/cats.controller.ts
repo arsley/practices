a51c83d44537da1fce24acb88d8f23cb2a28d27f
arsley <blue20will@gmail.com>
2019-05-08 13:22:49 +0900

    add: Read 'Provider' section

--- nest-practice/src/cats/cats.controller.ts
+++ nest-practice/src/cats/cats.controller.ts
@@ -1,19 +1,21 @@
 import { Controller, Get, Query, Post, Put, Delete, HttpCode, Param, Body } from '@nestjs/common';
 // import { Request } from 'express';
 import { CreateCatDto, UpdateCatDto, ListAllEntities } from './dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
   @Post()
-  create(@Body() createCatDto: CreateCatDto): string {
-    console.log('CREATE', createCatDto);
-    return 'This action adds a new cat';
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
   }
 
   @Get()
-  index(@Query() query: ListAllEntities) {
-    console.log('INDEX', query);
-    return 'This action returns all cats';
+  async index(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 
   // CAUTION : routing order
@@ -28,22 +30,22 @@ export class CatsController {
   // findWild() {
   //   return 'This route uses a wildcard';
   // }
-
-  @Get(':id')
-  show(@Param('id') id: string): string {
-    console.log('SHOW', id);
-    return `This action returns a #${id} cat`;
-  }
-
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto): string {
-    console.log('UPDATE', id, updateCatDto);
-    return `This action updates a #${id} cat`;
-  }
-
-  @Delete(':id')
-  destroy(@Param('id') id: string) {
-    console.log('DELETE', id);
-    return `This action removes a #${id} cat`;
-  }
+  //
+  // @Get(':id')
+  // show(@Param('id') id: string): string {
+  //   console.log('SHOW', id);
+  //   return `This action returns a #${id} cat`;
+  // }
+  //
+  // @Put(':id')
+  // update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto): string {
+  //   console.log('UPDATE', id, updateCatDto);
+  //   return `This action updates a #${id} cat`;
+  // }
+  //
+  // @Delete(':id')
+  // destroy(@Param('id') id: string) {
+  //   console.log('DELETE', id);
+  //   return `This action removes a #${id} cat`;
+  // }
 }
