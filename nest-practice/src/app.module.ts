1de870174aa4b6ce583c0dea09d1cd3a3415647c
arsley <blue20will@gmail.com>
2019-05-14 13:46:09 +0900

    :construction: Read 'Middleware' section with some fixes
    To use 'global middleware' this way, declare to use body-parser
    middleware

--- nest-practice/src/app.module.ts
+++ nest-practice/src/app.module.ts
@@ -1,7 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+// import { LoggerMiddleware } from './logger.middleware';
+// import { logger } from './logger.middleware';
 import { CatsModule } from './cats/cats.module';
+// import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [CatsModule],
 })
 export class AppModule {}
+
+// export class AppModule implements NestModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer
+//       .apply(logger)
+//       .forRoutes(CatsController);
+//       // .apply(LoggerMiddleware)
+//       // .exclude(
+//       //   { path: 'cats', method: RequestMethod.GET },
+//       //   { path: 'cats', method: RequestMethod.POST },
+//       // )
+//       // .forRoutes('cats');
+//       // .forRoutes({ path: 'cats', method: RequestMethod.GET });
+//   }
+// }
--- nest-practice/src/main.ts
+++ nest-practice/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import bodyParser = require('body-parser');
+import { logger } from './logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // ref: https://expressjs.com/ja/api.html#req.body
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use(logger);
   await app.listen(3000);
 }
 bootstrap();
