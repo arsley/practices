3a6cf638f19291297295f8bf94b8d3fc47ea1886
arsley <blue20will@gmail.com>
2019-06-15 00:11:33 +0900

    :zap: Read 'Set up Prisma'

---
+++ hello-prisma-go/index.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "context"
+    "fmt"
+
+    prisma "github.com/arsley/practices/hello-prisma-go/generated/prisma-client"
+)
+
+func main() {
+    client := prisma.New(nil)
+    ctx    := context.TODO()
+
+    // Create a new user
+    name := "Alice"
+    newUser, err := client.CreateUser(prisma.UserCreateInput{
+        Name: name,
+    }).Exec(ctx)
+
+    if err != nil { panic(err) }
+    fmt.Printf("Created new user: %+v\n", newUser)
+
+    users, err := client.Users(nil).Exec(ctx)
+    if err != nil { panic(err) }
+    fmt.Printf("%+v\n", users)
+}
