ccebff2161b18e5b1db8c83473b14d12897a4e7b
arsley <blue20will@gmail.com>
2019-06-18 07:30:33 +0900

    :pencil: Read 'Change Datamodel'

--- hello-prisma-go/index.go
+++ hello-prisma-go/index.go
@@ -7,20 +7,50 @@ import (
     prisma "github.com/arsley/practices/hello-prisma-go/generated/prisma-client"
 )
 
-func main() {
+func _main() {
     client := prisma.New(nil)
     ctx    := context.TODO()
 
-    // Create a new user
-    name := "Alice"
+    // Create a new user with tow Posts
+    name := "Bob"
+    email := "bob@prisma.io"
+    title1 := "Join up for GraphQL Conf in 2019"
+    title2 := "Sibscribe to GraphQL Weekly for GraphQL news"
     newUser, err := client.CreateUser(prisma.UserCreateInput{
         Name: name,
+        Email: &email,
+        Posts: &prisma.PostCreateManyWithoutAuthorInput{
+            Create: []prisma.PostCreateWithoutAuthorInput{
+                prisma.PostCreateWithoutAuthorInput{
+                  Title: title1,
+                },
+                prisma.PostCreateWithoutAuthorInput{
+                  Title: title2,
+                },
+            },
+        },
     }).Exec(ctx)
 
     if err != nil { panic(err) }
     fmt.Printf("Created new user: %+v\n", newUser)
 
-    users, err := client.Users(nil).Exec(ctx)
+    allUsers, err := client.Users(nil).Exec(ctx)
     if err != nil { panic(err) }
-    fmt.Printf("%+v\n", users)
+    fmt.Printf("%+v\n", allUsers)
+
+    allPosts, err := client.Posts(nil).Exec(ctx)
+    if err != nil { panic(err) }
+    fmt.Printf("%+v\n", allPosts)
+}
+
+func main() {
+    client := prisma.New(nil)
+    ctx    := context.TODO()
+
+    email := "bob@prisma.io"
+    postsByUser, err := client.User(prisma.UserWhereUniqueInput{
+        Email: &email,
+    }).Posts(nil).Exec(ctx)
+    if err != nil { panic(err) }
+    fmt.Printf("%+v\n", postsByUser)
 }
